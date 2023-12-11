bc4c0aa6cda61bdc2333907ed57f5cf2e573f5a2
Julius Liem <u7724204@anu.edu.au>
2023-12-11 18:05:44 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operator(firstNum, operator, secondNum) {
+    switch (operator) {
+        case "+":
+            return add(firstNum, secondNum);
+        case "-":
+            return subtract(firstNum, secondNum);
+        case "*":
+            return multiply(firstNum, secondNum);
+        case "/":
+            return divide(firstNum, secondNum);
+        default:
+            break;
+    }
+}
+
